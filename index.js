84a24178358bd80943d7e8007af23486eb5d4b68
Gabriel Anastacio <99836061+gabpereiraa@users.noreply.github.com>
2024-09-30 13:15:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const links = document.querySelectorAll('nav ul li a');
 const checkbox = document.getElementById('check');
 
-// Adicionar um evento de clique para cada link
 links.forEach(link => {
     link.addEventListener('click', () => {
         checkbox.checked = false; 
